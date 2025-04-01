a3ea037cfd7c0f42ca05dd9e0fcfc47062b4537b
phonghocode <ntp11892vt@gmail.com>
2025-04-01 08:13:48 +0700

    fix bug

--- baseline/engine.py
+++ baseline/engine.py
@@ -94,7 +94,7 @@ def evaluate(model, data_loader, device):
         model_time = time.time()
         outputs = model(images)
         loss_dict = model(images, targets)
-
+        print(loss_dict)
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = utils.reduce_dict(loss_dict)
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
