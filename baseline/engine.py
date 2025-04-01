832b41e6be66b4e0860d66c9257ab84ed31c14d0
phonghocode <ntp11892vt@gmail.com>
2025-04-01 14:03:36 +0700

    fix bug

--- baseline/engine.py
+++ baseline/engine.py
@@ -30,7 +30,6 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
         with torch.cuda.amp.autocast(enabled=scaler is not None):
             loss_dict = model(images, targets)
             losses = sum(loss for loss in loss_dict.values())
-        print(loss_dict)
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = utils.reduce_dict(loss_dict)
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
